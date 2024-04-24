1001209654cca5bba860f81b74b9499fe1ef05ee
tranngockhue2000 <khuetranngoc27700@gmail.com>
2024-04-25 00:04:51 +0700

    test push

--- src/main/java/com/example/docker/DockerApplication.java
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerApplication {
 
     public static void main(String[] args) {
+        System.out.println("test docker");
         SpringApplication.run(DockerApplication.class, args);
     }
 
